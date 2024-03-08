112f255a85b9cdc97b25d9480ffd7c61b32f9d58
weary06 <160468829+weary06@users.noreply.github.com>
2024-03-08 12:52:41 +0100

    continued the work

--- concessionario.py
+++ concessionario.py
@@ -1,6 +1,7 @@
-from abc import ABC
-class Concessionaria(ABC):
+class Concessionaria():
+    veicoli = []
     def __init__(self,targa,marca,modello,numeroDiPosti,prezzoDiBase):
+        Concessionaria.veicoli.append(self)
         self.set_targa(targa)
         self.set_marca(marca)
         self.set_modello(modello)
@@ -15,5 +16,51 @@ class Concessionaria(ABC):
         self._modello=modello
     def setNumeroDiPosti(self,numeroDiPosti):
         self._numeroDiPosti=numeroDiPosti
-    def getTarga(self):
+    def setPrezzoDiBase(self,prezzoDiBase):
+        self._prezzoDiBase=prezzoDiBase
+
+    def get_targa(self):
         return self._targa
+    def get_Marca(self):
+        return self._marca
+    def get_modello(self):
+        return self._modello
+    def get_numeroDiPosti(self):
+        return self._numeroDiPosti
+    def get_prezzoDiBase(self):
+        return self._prezzoDiBase
+
+    def stampa_Veicoli(self,veicoli):
+        print(veicoli)
+
+class Autocarro(Concessionaria):
+    def __init__(self,targa,marca,modello,numeroDiPosti,prezzoDiBase,capacitàDiCarico):
+        super().__init__(targa,marca,modello,numeroDiPosti,prezzoDiBase)
+        self.set_capacitàDiCarico(capacitàDiCarico)
+
+    def set_capacitàDiCarico(self,capacitàDiCarico):
+        self._capacitàDiCarico=capacitàDiCarico
+
+#Prezzo autocarro = prezzo di base x capacità massima di carico
+
+class Autoveicolo(Concessionaria):
+    def __init__(self,targa,marca,modello,numeroDiPosti,prezzoDiBase,numeroDiPorte):
+        super().__init__(targa,marca,modello,numeroDiPosti,prezzoDiBase)
+        self.set_numeroDiPorte(numeroDiPorte)
+
+    def set_NumeroDiPorte(self,numeroDiPorte):
+        self._numeroDiPorte = numeroDiPorte
+#Prezzo autoveicolo = prezzo di base x numero di porte
+
+class Motoveicolo(Concessionaria):
+    def __init__(self,targa,marca,modello,numeroDiPosti,prezzoDiBase,cilindrata):
+        super().__init__(targa,marca,modello,numeroDiPosti,prezzoDiBase)
+        self.set_cilindrata(cilindrata)
+
+    def set_Cilindrata(self,cilindrata):
+        self._cilindrata=cilindrata
+    def get_Cilindrata(self):
+        return self._cilindrata
+
+#Prezzo motoveicolo = prezzo di base x cilindrata
+
